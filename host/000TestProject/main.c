dc53c1eb2804395fd2484edceda919391b632a2b
minelli31 <minelli31@gmail.com>
2022-09-17 13:12:31 +0200

    Start Embedded C STM32

---
+++ host/000TestProject/main.c
@@ -0,0 +1,15 @@
+/*
+ * main.c
+ *
+ *  Created on: 16 sept. 2022
+ *      Author: minelli
+ */
+
+#include<stdio.h>
+
+int main(void){
+	printf("Hello World!");
+	printf("Press 'ENTER");
+	getchar();
+	return 0;
+}
